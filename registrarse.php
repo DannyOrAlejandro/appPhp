85619121033fb6613798e4011a0a8f95f3739e95
DannyOrAlejandro <dannya0993@gmail.com>
2021-10-25 19:56:08 -0500

    MEJORADO

--- registrarse.php
+++ registrarse.php
@@ -158,9 +158,6 @@ if(!empty($_POST) && isset($_REQUEST['link'])){
         <div class="alert"><?php echo isset($alert)?$alert:''; ?></div>
         <button type="reset" onclick="window.location='index.php';" class="btn btn-primary">Iniciar Sesion</button>
     </form>
-    <?php
-    include("mostrar.php");
-    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
